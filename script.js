62876ea951504dd6e8d5013ee75d9a4c1756b0e2
Jessalynn CHOBY <j.choby@proton.me>
2025-12-15 21:42:04 +0100

    Add script.js for hamburger menu functionality

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    const navMenu = document.querySelector('.nav-menu');
+    if (navMenu.style.display === 'block') {
+        navMenu.style.display = 'none';
+    } else {
+        navMenu.style.display = 'block';
+    }
+}
